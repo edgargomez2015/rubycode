df63dfb5cde512f893484bc31ecd500404317249
edgargomez <edgar.d.gomez@live.com>
2015-04-30 00:06:28 +0000

    madlibs

---
+++ height_to_cent.rb
@@ -0,0 +1,12 @@
+my_name = "Edgar"
+
+height_inches = 59
+
+weight_pounds = 213
+
+height_centimeters = height_inches * 2.54
+
+weight_kilograms = weight_pounds * 0.453592
+
+puts my_name + ' is ' + height_centimeters.to_s + ' cm and ' + weight_kilograms.to_s + ' kg.'
+
---
+++ madlib.rb
@@ -0,0 +1,8 @@
+puts "Hola! Como te llamas?"
+user_name = gets.chomp
+
+puts "Bienvenido al juego de madlib " + user_name + "!"
+puts "Tell me the name of your favorite snack: "
+snack = gets.chomp
+puts "What is your favorite color? "
+color = gets.chomp
